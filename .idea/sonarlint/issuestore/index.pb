c793f1751bb741ffb37db1b8e55f66a43867dde8
Kim <wkqkrla12@gmail.com>
2023-10-26 16:35:10 +0900

    editing index.js

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,6 @@
+
+9
+	index.css,6\7\674effb85dac479ec2f277de2d8e1a362611e177
+:
+
+hello.html,9\a\9a60b6907b5908143cfea0aa8f6f88cd6b9138df
--- index.js
+++ index.js
@@ -1,14 +1,53 @@
-const flipCard1 = document.getElementById("flip-card-1");
-const card1 = flipCard1.querySelector(".card");
+// const flipCard1 = document.getElementById("flip-card-1");
+// const card1 = flipCard1.querySelector(".card");
 
-const flipCard2 = document.getElementById("flip-card-2");
-const card2 = flipCard2.querySelector(".card");
+// const flipCard2 = document.getElementById("flip-card-2");
+// const card2 = flipCard2.querySelector(".card");
 
-flipCard1.addEventListener("click", (e) => {
-  card1.classList.toggle("back");
-});
+// flipCard1.addEventListener("click", (e) => {
+//   card1.classList.toggle("back");
+// });
 
-flipCard2.addEventListener("click", (e) => {
-  console.log("flipped");
-  card2.classList.toggle("back");
+// flipCard2.addEventListener("click", (e) => {
+//   console.log("flipped");
+//   card2.classList.toggle("back");
+// });
+
+// let flip = document.querySelector(".flip");
+// let front = document.querySelector(".project-front");
+// let back = document.querySelector(".project-back");
+
+// flip.addEventListener("click", (e) => {
+//   console.log("f");
+//   front.classList.toggle("project-back");
+// });
+
+let flip = document.querySelector(".flip");
+let card = flip.querySelector(".project-front");
+
+flip.addEventListener("click", (e) => {
+  card.classList.toggle("project-back");
+
+  if (card.className === "project-front project-back") {
+    console.log("back");
+    const helloDiv = document.createElement("div");
+    helloDiv.textContent = "hello";
+
+    const periodDiv = document.createElement("div");
+    periodDiv.textContent = "기간";
+
+    const partDiv = document.createElement("div");
+    partDiv.textContent = "한 파트";
+
+    const projectLinkDiv = document.createElement("div");
+    const link = document.createElement("a");
+    link.href = "https://wkqkrla12.github.io/portfolio/";
+    projectLinkDiv.appendChild(link);
+
+    // Append the created elements to the card's back side
+    project - front.appendChild(helloDiv);
+    back.appendChild(periodDiv);
+    back.appendChild(partDiv);
+    back.appendChild(projectLinkDiv);
+  }
 });
