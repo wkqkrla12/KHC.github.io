5b6447eafa9188076c03ea964c7856c15d7c7237
Kim <wkqkrla12@gmail.com>
2023-10-29 23:39:36 +0900

    add portfolio project and edited js & css

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ let item = document.querySelector(".projects-item");
 let back = null;
 let br = document.createElement("br");
 
+let flipPortfolio = document.querySelector(".flip-portfolio");
+let cardPortfolio = flipPortfolio.querySelector(".project-front-portfolio");
+let itemPortfolio = document.querySelector(".projects-item-portfolio");
+let backPortfolio = null;
+
 flip.addEventListener("click", (e) => {
   card.classList.toggle("project-back");
 
@@ -24,7 +29,7 @@ flip.addEventListener("click", (e) => {
       projectDiv.style.whiteSpace = "pre-line";
 
       let periodDiv = document.createElement("div");
-      periodDiv.textContent = "#기간 \n 23.04.24 ~ 23.07.19\n\n ";
+      periodDiv.textContent = "#기간 \n ::23.04.24 ~ 23.07.19\n\n ";
       periodDiv.style.width = "100%";
       periodDiv.style.fontSize = "1em";
       periodDiv.style.whiteSpace = "pre-line";
@@ -83,5 +88,76 @@ flip.addEventListener("click", (e) => {
   } else if (back) {
     item.removeChild(back);
     back = null;
+  }
+});
+
+flipPortfolio.addEventListener("click", (e) => {
+  cardPortfolio.classList.toggle("project-back-portfolio");
+
+  if (cardPortfolio.classList.contains("project-back-portfolio")) {
+    console.log("back (portfolio)");
+
+    if (!backPortfolio) {
+      backPortfolio = document.createElement("div");
+      backPortfolio.className = "project-back-portfolio";
+
+      let projectDivPortfolio = document.createElement("div");
+      projectDivPortfolio.textContent = "포트폴리오\n\n";
+      projectDivPortfolio.style.width = "100%";
+      projectDivPortfolio.style.fontSize = "18px";
+      projectDivPortfolio.style.fontWeight = "700";
+      projectDivPortfolio.style.color = "#4c4c6d";
+      projectDivPortfolio.style.whiteSpace = "pre-line";
+
+      let techDivPortfolio = document.createElement("div");
+      techDivPortfolio.textContent = "#Tech\n :: JavaScript, HTML, CSS\n\n";
+      techDivPortfolio.style.width = "100%";
+      techDivPortfolio.style.fontSize = "1em";
+      techDivPortfolio.style.whiteSpace = "pre-line";
+
+      let partDivWrapPortfolio = document.createElement("div");
+      partDivWrapPortfolio.textContent = "#My Work \n";
+      partDivWrapPortfolio.style.width = "100%";
+      partDivWrapPortfolio.style.fontSize = "1em";
+
+      let partDivPortfolio = document.createElement("div");
+      partDivPortfolio.textContent = ":: Design, Frontend\n\n";
+      partDivPortfolio.style.width = "100%";
+      partDivPortfolio.style.fontSize = "1em";
+      partDivPortfolio.style.whiteSpace = "pre-line";
+
+      let projectLinkDivPortfolio = document.createElement("div");
+      let linkPortfolio = document.createElement("a");
+      linkPortfolio.href = "https://wkqkrla12.github.io/portfolio/";
+      projectLinkDivPortfolio.style.width = "290px";
+      projectLinkDivPortfolio.style.height = "30px";
+      projectLinkDivPortfolio.style.position = "relative";
+      projectLinkDivPortfolio.style.whiteSpace = "pre-line";
+
+      linkPortfolio.style.position = "absolute";
+      linkPortfolio.style.backgroundColor = "#ffc3c3";
+      linkPortfolio.style.borderRadius = "12px";
+      linkPortfolio.style.color = "#fff";
+      linkPortfolio.style.cursor = "pointer";
+      linkPortfolio.style.marginRight = "10px";
+      linkPortfolio.style.padding = "4px 8px";
+      linkPortfolio.style.width = "40%";
+      linkPortfolio.style.height = "70%";
+      linkPortfolio.style.textAlign = "center";
+      linkPortfolio.style.justifyItems = "center";
+      linkPortfolio.textContent = "Source Code";
+      projectLinkDivPortfolio.appendChild(linkPortfolio);
+
+      backPortfolio.appendChild(projectDivPortfolio);
+      backPortfolio.appendChild(techDivPortfolio);
+      backPortfolio.appendChild(partDivWrapPortfolio);
+      backPortfolio.appendChild(partDivPortfolio);
+      backPortfolio.appendChild(projectLinkDivPortfolio);
+
+      itemPortfolio.appendChild(backPortfolio);
+    }
+  } else if (backPortfolio) {
+    itemPortfolio.removeChild(backPortfolio);
+    backPortfolio = null;
   }
 });
